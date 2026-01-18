a9850df9be96c7be641f07fa231d98465fdf17c0
helalferrari <ferrari.work@gmail.com>
2026-01-18 18:59:33 -0300

    Primeiro commit com pacote do spring, com as 4 dependencias basicas, spring devtools, web, JPA e JDBC

---
+++ src/main/java/com/helalferrari/subscribeEvents/SubscribeEventsApplication.java
@@ -0,0 +1,13 @@
+package com.helalferrari.subscribeEvents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubscribeEventsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubscribeEventsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/helalferrari/subscribeEvents/SubscribeEventsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helalferrari.subscribeEvents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubscribeEventsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
